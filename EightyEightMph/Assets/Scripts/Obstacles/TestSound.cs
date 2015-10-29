6f29dec44b0640c9a3b52fd0b55564fb32420ef3
JeremyJacquemont <jeremy.jacquemont@gmail.com>
2015-10-29 02:23:37 +0100

    Add Sound

---
+++ EightyEightMph/Assets/Scripts/Obstacles/TestSound.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestSound : MonoBehaviour {
+
+	[Range(0.00001f,1)]
+	public float distance;
+
+	public SoundObstacle target;
+
+	void Update () {
+		target.SetVolume (distance);
+	}
+}
