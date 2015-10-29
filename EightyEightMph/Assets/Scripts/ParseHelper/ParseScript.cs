24f3e7d67692108a25487ed9fc9c0d52d6cdaf46
JeremyJacquemont <jeremy.jacquemont@gmail.com>
2015-10-29 06:34:45 +0100

    Add SaveScore

---
+++ EightyEightMph/Assets/Scripts/ParseHelper/ParseScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using Parse;
+
+public class ParseScript : MonoBehaviour {
+
+	void Start() {
+		ParseScript.SaveScore (37373);
+	}
+
+	public static void SaveScore(int score) {
+
+		ParseObject gameScore = new ParseObject("Score");
+		gameScore["Score"] = score;
+		gameScore.SaveAsync ().ContinueWith (t => {
+			Debug.Log(gameScore.ObjectId);
+		});
+
+	}
+
+}
